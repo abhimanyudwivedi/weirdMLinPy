8bc06f16c2136c2c093c3576864a480f2f9eab03
Abhimanyu Dwivedi <abhimanyudwivedi@gmail.com>
2019-02-19 16:54:35 +0530

    Random Forest Regressor, I'm bummed Forrest is spelled as Forest, pretty nice algo though

---
+++ RFR.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 19 16:35:03 2019
+
+@author: Abhimanyu_Dwivedi
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#dataset
+
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2:3].values
+
+#creating the regressor
+from sklearn.ensemble import RandomForestRegressor
+reg = RandomForestRegressor(n_estimators = 250, random_state = 0)
+reg.fit(X, y)
+
+#prediction
+
+y_pred = reg.predict(6.5)
+
+#visualize with high resolution
+X_grid = np.arange(min(X), max(X), 0.01)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, reg.predict(X_grid), color = 'blue')
+plt.title('Truth or Bluff')
+plt.xlabel('Position')
+plt.ylabel('Salaries')
+plt.show()
