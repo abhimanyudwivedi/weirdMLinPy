517e7f021ecafb4208f88bc3902569b04eab6214
Abhimanyu Dwivedi <abhimanyudwivedi@gmail.com>
2019-02-28 02:41:48 +0530

    Random Forest Regressor

--- RFR.py
+++ RFR.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+ # -*- coding: utf-8 -*-
 """
 Created on Tue Feb 19 16:35:03 2019
 
@@ -17,18 +17,18 @@ y = dataset.iloc[:, 2:3].values
 
 #creating the regressor
 from sklearn.ensemble import RandomForestRegressor
-reg = RandomForestRegressor(n_estimators = 250, random_state = 0)
-reg.fit(X, y)
+ref = RandomForestRegressor(n_estimators = 250, random_state = 0)
+ref.fit(X, y)
 
 #prediction
 
-y_pred = reg.predict(6.5)
+y_pred = ref.predict(6.5)
 
 #visualize with high resolution
 X_grid = np.arange(min(X), max(X), 0.01)
 X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, y, color = 'red')
-plt.plot(X_grid, reg.predict(X_grid), color = 'blue')
+plt.plot(X_grid, ref.predict(X_grid), color = 'blue')
 plt.title('Truth or Bluff')
 plt.xlabel('Position')
 plt.ylabel('Salaries')
